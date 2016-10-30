0b42eebac7b24c884ee9d8c27af2d7fc04671639
Machtan <jako3047@gmail.com>
2016-10-30 12:38:41 +0100

    Add SDL_TOUCH_MOUSEID

--- sdl2-sys/src/touch.rs
+++ sdl2-sys/src/touch.rs
@@ -1,9 +1,12 @@
-use libc::{c_int, int64_t};
+use libc::{c_int, int64_t, uint32_t};
+use std::u32;
 
 pub type SDL_TouchID = int64_t;
 pub type SDL_FingerID = int64_t;
 pub type SDL_Finger = Finger;
 pub type TouchDevice = SDL_TouchID;
+
+pub const SDL_TOUCH_MOUSEID: uint32_t = u32::MAX;
 
 #[derive(PartialEq, Copy, Clone)]
 #[repr(C)]
@@ -18,6 +21,5 @@ extern "C" {
     pub fn SDL_GetNumTouchDevices() -> c_int;
     pub fn SDL_GetTouchDevice(index: c_int) -> SDL_TouchID;
     pub fn SDL_GetNumTouchFingers(touchID: SDL_TouchID) -> c_int;
-    pub fn SDL_GetTouchFinger(touchID: SDL_TouchID, index: c_int) ->
-              *mut SDL_Finger;
+    pub fn SDL_GetTouchFinger(touchID: SDL_TouchID, index: c_int) -> *mut SDL_Finger;
 }
